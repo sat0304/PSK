aa2ed0e7a425972839002e246a673e4db553af33
Сергей Токарев <sat0304@yandex.ru>
2022-01-13 11:29:12 +0400

    toolbar Django was added

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -33,6 +33,9 @@ CACHES = {
     }
 }
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
 
 """Application definition."""
 
@@ -48,6 +51,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf import settings
 
 from django.urls import include, path
 
@@ -37,3 +38,7 @@ urlpatterns = [
         )
     ),
 ]
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
